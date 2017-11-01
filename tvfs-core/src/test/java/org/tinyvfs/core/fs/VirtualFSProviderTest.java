0460d14a80f55b21ded58bf5e3ace3ca89265fe5
abarhub <abarhub@github.com>
2017-11-01 11:49:38 +0100

    reorganisation du code

--- tvfs-core/src/test/java/org/tinyvfs/core/fs/VirtualFSProviderTest.java
+++ tvfs-core/src/test/java/org/tinyvfs/core/fs/VirtualFSProviderTest.java
@@ -4,16 +4,18 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import org.tinyvfs.core.config.TVFSConfigParam;
+import org.tinyvfs.core.TVFSTools;
 import org.tinyvfs.core.config.TVFSRepository;
 import org.tinyvfs.core.path.TVFSAbstractPath;
-import org.tinyvfs.core.path.TVFSRootName;
 
+import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystemAlreadyExistsException;
 import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 import static org.tinyvfs.core.TVFSTools.toList;
@@ -27,35 +29,46 @@ public class VirtualFSProviderTest {
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
 
+	private VirtualFSProvider virtualFSProvider;
 
 	@Before
 	public void init() throws Exception {
 		TVFSRepository.clearInstance();
+		virtualFSProvider = new VirtualFSProvider();
 	}
 
 	@Test
 	public void testGetSchemeOK() throws Exception {
 		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
-		assertEquals("tvfs", virtualFSProvider.getScheme());
+		// methode testée
+		String res = virtualFSProvider.getScheme();
+
+		// vérifications
+		assertEquals("tvfs", res);
 		assertEquals("tvfs", VirtualFSProvider.SCHEME);
+		assertEquals(VirtualFSProvider.SCHEME, res);
 	}
 
 	@Test
 	public void testNewFileSystemOK() throws Exception {
-		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
-		FileSystem fs = virtualFSProvider.newFileSystem(URI.create("tvfs://test1"), null);
+		// methode testée
+		FileSystem fs = createFileSystem("test1", null);
+
+		// vérifications
 		assertNotNull(fs);
 		assertTrue(fs instanceof TVFileSystem);
+		assertEquals("test1", ((TVFileSystem) fs).getName().getName());
 	}
 
 	@Test
 	public void testNewFileSystem2FoisKO() throws Exception {
-		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
-		FileSystem fs = virtualFSProvider.newFileSystem(URI.create("tvfs://test1"), null);
+		FileSystem fs = createFileSystem("test2", null);
 		assertNotNull(fs);
 		assertTrue(fs instanceof TVFileSystem);
+		assertEquals("test2", ((TVFileSystem) fs).getName().getName());
 		try {
-			fs = virtualFSProvider.newFileSystem(URI.create("tvfs://test1"), null);
+			// methode testée
+			fs = createFileSystem("test2", null);
 			fail("Error");
 		} catch (FileSystemAlreadyExistsException e) {
 			assertEquals("Le FS existe déjà", e.getMessage());
@@ -64,41 +77,44 @@ public class VirtualFSProviderTest {
 
 	@Test
 	public void testGetFileSystemOK() throws Exception {
-		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
-		URI uri = URI.create("tvfs://test1");
-		FileSystem fs = virtualFSProvider.newFileSystem(uri, null);
+		URI uri = createUri("test1");
+		FileSystem fs = createFileSystem(uri);
 		assertNotNull(fs);
 		assertTrue(fs instanceof TVFileSystem);
 
+		// methode testée
 		FileSystem fs2 = virtualFSProvider.getFileSystem(uri);
+
+		// vérifications
 		assertNotNull(fs2);
 		assertTrue(fs == fs2);
 	}
 
 	@Test
 	public void testGetFileSystemFSInexistantKO() throws Exception {
-		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
-		URI uri = URI.create("tvfs://test1");
+		URI uri = createUri("test1");
 
 		try {
+			// methode testée
 			FileSystem fs2 = virtualFSProvider.getFileSystem(uri);
 			fail("Error");
 		} catch (FileSystemNotFoundException e) {
-			assertEquals("Le FS n'existe pas", e.getMessage());
+			assertEquals("Le FS 'test1' n'existe pas", e.getMessage());
 		}
 	}
 
 	@Test
 	public void testGetPathOK() throws Exception {
-		URI uri = URI.create("tvfs://test1/aaa/bbb/ccc");
-		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
+		URI uri = createUri("test1", "/aaa/bbb/ccc");
 
-		FileSystem fs = virtualFSProvider.newFileSystem(uri, null);
+		FileSystem fs = createFileSystem(uri);
 		assertNotNull(fs);
 		assertTrue(fs instanceof TVFileSystem);
-		TVFileSystem fs2 = (TVFileSystem) fs;
-		virtualFSProvider.add(new TVFSConfigParam(new TVFSRootName("test1"), folder.newFolder().toPath(), true));
+
+		// methode testée
 		Path path = virtualFSProvider.getPath(uri);
+
+		// vérifications
 		assertNotNull(path);
 		assertTrue(path instanceof TVFSAbstractPath);
 
@@ -114,15 +130,17 @@ public class VirtualFSProviderTest {
 
 	@Test
 	public void testGetPathSlashDoubleOK() throws Exception {
-		URI uri = URI.create("tvfs://test1///aaaa/bbbbbb//cccc//dddd");
-		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
+		URI uri = createUri("test1", "///aaaa/bbbbbb//cccc//dddd");
 
-		FileSystem fs = virtualFSProvider.newFileSystem(uri, null);
+		FileSystem fs = createFileSystem(uri);
 		assertNotNull(fs);
 		assertTrue(fs instanceof TVFileSystem);
 		TVFileSystem fs2 = (TVFileSystem) fs;
-		virtualFSProvider.add(new TVFSConfigParam(new TVFSRootName("test1"), folder.newFolder().toPath(), true));
+
+		// methode testée
 		Path path = virtualFSProvider.getPath(uri);
+
+		// vérifications
 		assertNotNull(path);
 		assertTrue(path instanceof TVFSAbstractPath);
 
@@ -139,15 +157,19 @@ public class VirtualFSProviderTest {
 
 	@Test
 	public void testGetPathSlashFinOK() throws Exception {
-		URI uri = URI.create("tvfs://test1/aaaa/bbbbbb/cccc/dddd/");
-		VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
+		URI uri = createUri("test1", "/aaaa/bbbbbb/cccc/dddd/");
+		//VirtualFSProvider virtualFSProvider = new VirtualFSProvider();
 
-		FileSystem fs = virtualFSProvider.newFileSystem(uri, null);
+		FileSystem fs = createFileSystem(uri);
 		assertNotNull(fs);
 		assertTrue(fs instanceof TVFileSystem);
 		TVFileSystem fs2 = (TVFileSystem) fs;
-		virtualFSProvider.add(new TVFSConfigParam(new TVFSRootName("test1"), folder.newFolder().toPath(), true));
+		//virtualFSProvider.add(new TVFSConfigParam(new TVFSRootName("test1"), folder.newFolder().toPath(), true));
+
+		// methode testée
 		Path path = virtualFSProvider.getPath(uri);
+
+		// vérifications
 		assertNotNull(path);
 		assertTrue(path instanceof TVFSAbstractPath);
 
@@ -162,5 +184,33 @@ public class VirtualFSProviderTest {
 		assertEquals("test1:/aaaa/bbbbbb/cccc/dddd", p.toString());
 	}
 
+	// methodes utilitaires
+
+	private URI createUri(String name) {
+		return TVFSTools.createURI(name, null);
+	}
+
+	private URI createUri(String name, String path) {
+		return TVFSTools.createURI(name, path);
+	}
+
+	private FileSystem createFileSystem(String name, String path) throws IOException {
+		return createFileSystem(createUri(name, path));
+	}
+
+	private FileSystem createFileSystem(URI uri) throws IOException {
+		return virtualFSProvider.newFileSystem(uri, createEnv());
+	}
+
+	private Map<String, String> createEnv(Path rootPath) {
+		Map<String, String> map = new HashMap<>();
+		map.put(VirtualFSProvider.ROOT_PATH, rootPath.toString());
+		map.put(VirtualFSProvider.READ_ONLY, "false");
+		return map;
+	}
+
+	private Map<String, String> createEnv() throws IOException {
+		return createEnv(folder.newFolder().toPath());
+	}
 
 }
